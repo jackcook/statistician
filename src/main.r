f2d2ba7f2b9dc2c3853f53f59b10da688299aa44
Jack Cook <hello@jackcook.nyc>
2016-12-03 18:23:59 -0500

    Added pie chart for categorical data

--- src/main.r
+++ src/main.r
@@ -3,7 +3,7 @@ setwd("/Users/jackcook/Desktop/statistician/src")
 source("bivariate.r")
 source("univariate.r")
 
-data_type <- readline(prompt = "What type of data would you like to analyze? (1 = univariate, 2 = bivariate): ")
+data_type <- readline(prompt = "What type of data would you like to analyze? (1 = univariate, 2 = bivariate, 3 = categorical): ")
 
 if (data_type == 1) {
   name <- readline(prompt = "What is being measured? ")
@@ -24,4 +24,19 @@ if (data_type == 1) {
   data <- matrix(cbind(explanatory_values, response_values), ncol = 2)
   analysis <- analyze_bivariate_data(data, explanatory_name, response_name)
   print(analysis)
+} else if (data_type == 3) {
+  categories_name <- readline(prompt = "What are your categories? ")
+  categories <- readline(prompt = "Enter in your category names, separated by spaces: ")
+  values <- readline(prompt = "Enter in your categories' values, separated by spaces: ")
+  
+  category_names <- strsplit(categories, " ")[[1]]
+  category_values <- as.numeric(strsplit(values, " ")[[1]])
+  
+  percentages <- round(category_values / sum(category_values) * 100)
+  
+  lbls <- category_names
+  lbls <- paste(lbls, percentages)
+  lbls <- paste(lbls, "%", sep = "")
+  
+  pie(category_values, labels = lbls, main = categories_name)
 }
